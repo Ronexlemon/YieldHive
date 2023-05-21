ad7cfd2c8d55c18a0fdb0e7121be124084fee672
RonexLemon <ronexondimu@gmail.com>
2023-05-21 11:25:57 +0300

    implement navbar

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavBar = ()=>{
+    return(
+        <div className="h-10 w-full bg-gray-300 flex justify-around items-center">
+            <div>
+                YieldHive
+            </div>
+            <div>
+                About
+            </div>
+            <div>
+                Connect
+            </div>
+
+        </div>
+
+    )
+}
+export default NavBar;
