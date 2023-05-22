6f9e9c9fe3745ffc31fd61daa5be8a401955b6cf
RonexLemon <ronexondimu@gmail.com>
2023-05-22 21:04:55 +0300

    fix connect button

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -78,7 +78,11 @@ const NavBar = () => {
           )}
         </div>
         {/* <button  className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Connect</button> */}
-        <ConnectButton label="Connect" chainStatus="icon"/>
+        
+        <ConnectButton label="Connect" chainStatus="icon"  showBalance={{
+    smallScreen: false,
+    largeScreen: true,
+  }}  />
       </div>
     </nav>
   );
