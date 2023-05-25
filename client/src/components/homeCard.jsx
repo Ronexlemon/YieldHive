69e35ee9730ba2266ffc4f8ea18cadea9cde4a8f
RonexLemon <ronexondimu@gmail.com>
2023-05-25 20:55:25 +0300

    final commit

--- client/src/components/homeCard.jsx
+++ client/src/components/homeCard.jsx
@@ -8,9 +8,12 @@ const HomeCard = () => {
     <div className="inset-0 flex justify-center mt-10 h-3/4">
       <div className="w-3/4 p-6 border border-gray-400 rounded-lg shadow bg-bgbank dark:bg-gray-800 dark:border-gray-700" id="open-btn">
        
-
-       
+<div className="flex justify-center items-center h-3/4"><span className=" text-7xl animate-ping text-black">YIELDHIVE</span></div>
+<div className="flex justify-center items-center">
+        <span className="text-white text-5xl">Unlock Your Hive</span>
       </div>
+      </div>
+     
 
       
     </div>
